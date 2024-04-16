7def71a7ca0ba0b311ec324c88160966da3afdcb
Marya <maryaeduarda15059927@gmail.com>
2024-04-16 11:52:15 -0300

    imagens

---
+++ Marya2/Results.cs
@@ -0,0 +1,20 @@
+using System.Dynamic;
+
+namespace Marya2;
+
+public class results 
+{
+    public int temp {get;set;}
+    public string discription {get;set;}
+    public string city {get;set;}
+    public double rain {get;set;}
+    public int humiditx {get;set;}
+    public string sunrise {get;set;}
+    public string sunset {get;set;}
+    public string wind_speedy {get;set;}
+    public int wind_direction {get;set;}
+    public string moon_phase {get;set;}
+    public string currently {get;set;}
+    public int img_id {get;set;}
+    public string codition_code {get;set;}
+}
