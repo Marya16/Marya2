267171cab50252c07b247f4552bb0a9d330f0992
Marya <maryaeduarda15059927@gmail.com>
2024-04-24 11:44:03 -0300

    lindassss

--- Marya2/MainPage.xaml.cs
+++ Marya2/MainPage.xaml.cs
@@ -2,10 +2,32 @@
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
+	results Resultado;
+	public MainPage() 
 	{
+		Resultado= new results();
 		InitializeComponent();
+		TestandoLayout();
+		PreencherTela();
 	}
-
+	void TestandoLayout()
+	{
+		Resultado.temp=32;
+		Resultado.city="Apucarana, PR";
+		Resultado.description="Ensolarado";
+		Resultado.rain=0;
+		Resultado.humidity=2%;
+		Resultado.sunrise="5:40 am";
+		Resultado.sunset="18:50 pm";
+		Resultado.wind_speedy="3.99 km/h";
+		Resultado.wind_direction=30;
+		Resultado.moon_phase="Nova";
+		Resultado.currently="Dia";
+		Resultado.codition_code="25";
+		Resultado.img_id="26";
+		Resultado.cloudiness=14;
+		Resultado.wind_cardinal="S";
+	
+	}
 }
 
